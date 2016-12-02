f5f3ae789fb5b781e3d07ea154ef20ed92e69f44
Andrey Litvinov <litwinoff@gmail.com>
2016-12-02 18:36:51 +0300

    Simple example

---
+++ DesignPatterns/src/delegation/simple/Main.java
@@ -0,0 +1,14 @@
+package delegation.simple;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Boss president = new Boss();
+		
+		president.clearSnow();
+		president.goForBeer();
+
+	}
+
+}
---
+++ DesignPatterns/src/delegation/simple/Padawan.java
@@ -0,0 +1,11 @@
+package delegation.simple;
+
+public class Padawan {
+	
+	void clearSnow() {
+		System.out.println("Padawan чистит снег");
+	}
+	void goForBeer() {
+		System.out.println("Padawan бежит за пивом");
+	}
+}
